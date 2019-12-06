05a42ff5167ad305ef0252c27a8bf5abee8fdfdd
EsraaSaad298 <esraa.abdo@student.guc.edu.eg>
2019-12-07 02:20:21 +0400

    A buyer can book a car as long as it is in stock, Close #5

--- services/buyerService.js
+++ services/buyerService.js
@@ -10,6 +10,7 @@ class BuyerService {
     }
     
 
+
     saveCar(Id, userId, save, callback){
         let dbQuery = this.db.collection("Cars").doc(Id);
     dbQuery
@@ -49,6 +50,43 @@ class BuyerService {
       .catch((err) => console.error(err));
        
     } 
+
+    bookCar(Id, userId, callback){
+      let dbQuery = this.db.collection("Cars").doc(Id);
+      dbQuery
+        .get()
+        .then((doc) => {
+          if (doc.exists) {
+            if(doc.get('Stock') != 0){
+              let id = userId.toString(2);
+              let dbUser = this.db.collection("Users").doc(id); 
+              dbUser
+             .get()
+             .then((document) => {
+                 if(document.exists){
+                  let currentData = doc.data();
+                  currentData.token = Id;
+                    console.log(currentData);
+                      dbUser.update({
+                      "Booked Cars" : admin.firestore.FieldValue.arrayUnion(currentData)
+                      }).then((document) => {
+                        callback({Info: "Document successfully updated!"});
+                      });    
+                    
+                 }else {
+                  callback({ Error: "Document User ID does not exist" });
+                }
+             })
+             .catch((err) => console.error(err));
+            }else{
+              callback({ Error: "This car has just went out of stock!" });
+            }
+          } else {
+            callback({ Error: "Document Car ID does not exist" });
+          }
+          })
+          .catch((err) => console.error(err));
+    }
 }  
 module.exports = (app, firebase, serviceManager) => {
     let buyerService = new BuyerService(app, firebase);
