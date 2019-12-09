b9953939231d005f7112ea8787b3f0bd520aacbb
Omar I. Handouk <omar.handouk@yandex.com>
2019-12-09 09:58:06 +0200

    Formatting & Package-lock.json reload

--- services/adminServices.js
+++ services/adminServices.js
@@ -1,128 +1,130 @@
-"use strict";
+'use strict';
 
 class AdminService {
-  constructor(app, firebase) {
-    this.app = app;
-    this.firebase = firebase;
-    this.db = this.firebase.firestore();
-  }
+	constructor(app, firebase) {
+		this.app = app;
+		this.firebase = firebase;
+		this.db = this.firebase.firestore();
+	}
 
-  getPendingRequests(callback) {
-    let collectionRef = this.db.collection("BookingRequests");
+	getPendingRequests(callback) {
+		let collectionRef = this.db.collection('BookingRequests');
 
-    collectionRef
-      .where("status", "==", "pending payment")
-      .get()
-      .then((snapshot) => {
-        let requests = [];
+		collectionRef
+			.where('status', '==', 'pending payment')
+			.get()
+			.then((snapshot) => {
+				let requests = [];
 
-        if (!snapshot.empty) {
-          snapshot.forEach((doc) => {
-            requests.push(doc.data());
-          });
-        }
+				if (!snapshot.empty) {
+					snapshot.forEach((doc) => {
+						requests.push(doc.data());
+					});
+				}
 
-        callback(requests);
-      })
-      .catch((err) => console.error(err));
-  }
+				callback(requests);
+			})
+			.catch((err) => console.error(err));
+	}
 
-  updateRequestStatus(requestId, callback) {
-    let collectionRef = this.db.collection("BookingRequests");
+	updateRequestStatus(requestId, callback) {
+		let collectionRef = this.db.collection('BookingRequests');
 
-    let updateDoc = {
-      status: "down payment provided"
-    };
+		let updateDoc = {
+			status: 'down payment provided'
+		};
 
-    collectionRef.doc(requestId).update(updateDoc);
+		collectionRef.doc(requestId).update(updateDoc);
 
-    collectionRef
-      .doc(requestId)
-      .get()
-      .then((doc) => {
-        let orderToken = doc.data().token;
-        let userId = doc.data().userId;
+		collectionRef
+			.doc(requestId)
+			.get()
+			.then((doc) => {
+				let orderToken = doc.data().token;
+				let userId = doc.data().userId;
 
-        this.db
-          .collection("Users")
-          .doc(userId)
-          .get()
-          .then((doc) => {
-            let booked = doc.data().bookedCars;
+				this.db
+					.collection('Users')
+					.doc(userId)
+					.get()
+					.then((doc) => {
+						let booked = doc.data().bookedCars;
 
-            let bookingRequests = [];
+						let bookingRequests = [];
 
-            booked.forEach((doc) => {
-              if (doc.token === orderToken) {
-                doc["status"] = "down payment provided";
-              }
+						booked.forEach((doc) => {
+							if (doc.token === orderToken) {
+								doc['status'] = 'down payment provided';
+							}
 
-              bookingRequests.push(doc);
-            });
+							bookingRequests.push(doc);
+						});
 
-            this.db
-              .collection("Users")
-              .doc(userId)
-              .update({ bookedCars: bookingRequests })
-              .then(() =>
-                callback({ Info: `Order ${requestId} status has been updated` })
-              )
-              .catch((err) => console.error(err));
-          })
-          .catch((err) => console.error(err));
-      })
-      .catch((err) => console.error(err));
-  }
+						this.db
+							.collection('Users')
+							.doc(userId)
+							.update({ bookedCars: bookingRequests })
+							.then(() =>
+								callback({ Info: `Order ${requestId} status has been updated` })
+							)
+							.catch((err) => console.error(err));
+					})
+					.catch((err) => console.error(err));
+			})
+			.catch((err) => console.error(err));
+	}
 
-  deleteRequest(requestId, callback) {
-    let collectionRef = this.db.collection("BookingRequests");
+	deleteRequest(requestId, callback) {
+		let collectionRef = this.db.collection('BookingRequests');
 
-    // TODO: Delete order from user
+		// TODO: Delete order from user
 
-    collectionRef
-      .doc(requestId)
-      .delete()
-      .then(() => callback({ Info: `Order ${requestId} has been deleted successfully` }))
-      .catch(err => console.error(err));
+		collectionRef
+			.doc(requestId)
+			.delete()
+			.then(() =>
+				callback({ Info: `Order ${requestId} has been deleted successfully` })
+			)
+			.catch((err) => console.error(err));
 
-    collectionRef
-      .doc(requestId)
-      .get()
-      .then((doc) => {
-        let orderToken = doc.data().token;
-        let userId = doc.data().userId;
+		collectionRef
+			.doc(requestId)
+			.get()
+			.then((doc) => {
+				let orderToken = doc.data().token;
+				let userId = doc.data().userId;
 
-        this.db
-          .collection("Users")
-          .doc(userId)
-          .get()
-          .then((doc) => {
-            let booked = doc.data().bookedCars;
+				this.db
+					.collection('Users')
+					.doc(userId)
+					.get()
+					.then((doc) => {
+						let booked = doc.data().bookedCars;
 
-            let bookingRequests = [];
+						let bookingRequests = [];
 
-            booked.forEach((doc) => {
-              if (doc.token !== orderToken) {
-                bookingRequests.push(doc);
-              }
-            });
+						booked.forEach((doc) => {
+							if (doc.token !== orderToken) {
+								bookingRequests.push(doc);
+							}
+						});
 
-            this.db
-              .collection("Users")
-              .doc(userId)
-              .update({ bookedCars: bookingRequests })
-              .then(() =>
-                callback({ Info: `Order ${requestId} status has been deleted` })
-              )
-              .catch((err) => console.error(err));
-          })
-          .catch((err) => console.error(err));
-      })
-      .catch((err) => console.error(err));
-  }
+						this.db
+							.collection('Users')
+							.doc(userId)
+							.update({ bookedCars: bookingRequests })
+							.then(() =>
+								callback({ Info: `Order ${requestId} status has been deleted` })
+							)
+							.catch((err) => console.error(err));
+					})
+					.catch((err) => console.error(err));
+			})
+			.catch((err) => console.error(err));
+	}
 }
 
 module.exports = (app, firebase, serviceManager) => {
-  let adminService = new AdminService(app, firebase);
-  serviceManager.set("adminService", adminService);
+	let adminService = new AdminService(app, firebase);
+	serviceManager.set('adminService', adminService);
 };
